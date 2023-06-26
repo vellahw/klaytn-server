823176c461c1c29ec9a799172d811e2957205007
Vella <gksdnjs2@gmail.com>
2023-06-26 14:31:49 +0900

    회원가입 페이지, 처리 api 작성

--- routes/main.js
+++ routes/main.js
@@ -19,7 +19,24 @@ const token = require('../token/kip7.js')
 
 module.exports = ()=>{
     // 기본 경로 localhost:3000/
-    router.get('/', (req, res)=>{
+    router.get('/', (req, res)=>
         res.render('login.ejs')
+    )
+
+    // 회원가입 페이지
+    router.get('/signup', (req, res)=>
+        res.render('signup.ejs')
+    )
+
+    // 유저가 입력한 정보를 mysql db에 삽입
+    router.post('/signup', async (req, res)=>{
+        // 유저가 입력한 정보를 변수에 대입, 확인
+        const input_phone = req.body._phone
+        const input_pass = req.body._pass
+        console('-> 회원가입 정보: ', input_phone, input_pass)
+        const wallet = await token.create_wallet()
+        console.log('-> 지갑 생성 됐을까욤: ', wallet)
+        
     })
+
 }
